213700f27d6fa69126ac5cd7b1291a1e4dccb925
Regis Hastings <25518602+regis-hastings@users.noreply.github.com>
2020-05-24 16:12:35 -0700

    add caporal and fs

--- index.js
+++ index.js
@@ -2,13 +2,30 @@
 
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
+const program = require('caporal');
+const fs = require('fs');
 
-const start = debounce(() => {
-  console.log("STARTING USER'S PROGRAM");
-}, 100);
+program
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action(async ({ filename }) => {
+    const name = filename || 'index.js';
 
-chokidar
-  .watch('.')
-  .on('add', () => start)
-  .on('change', () => console.log('FILE CHANGED'))
-  .on('unlink', () => console.log('FILE UNLINKED'));
+    try {
+      await fs.promises.access(name);
+    } catch (err) {
+      throw new Error(`Could not find the file ${name}`);
+    }
+
+    const start = debounce(() => {
+      console.log("STARTING USER'S PROGRAM");
+    }, 100);
+
+    chokidar
+      .watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+
+program.parse(process.argv);
